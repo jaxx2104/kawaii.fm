c08ac82fe5ca93382a100daac90f57c20af5df84
jaxx2104 <jaxx2104@gmail.com>
2019-07-08 22:29:32 +0900

    :arrow_up: Upgrade next.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withTypescript = require("@zeit/next-typescript")
 const withImages = require("next-images")
 
 module.exports = (phase, { defaultConfig }) => {
   // console.log(defaultConfig)
-  return withTypescript(
-    withImages({
-      distDir: "../dist"
-    })
-  )
+  return withImages({
+    distDir: "../dist"
+  })
 }
